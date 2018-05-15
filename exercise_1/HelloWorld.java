c3c3517ec2c12cc83cee7eaf9b282d9b01653f8d
leoni0301 <38730364+leoni0301@users.noreply.github.com>
2018-05-15 14:24:29 +0200

    upload files excercise 1

---
+++ exercise_1/HelloWorld.java
@@ -0,0 +1,8 @@
+// file name == class name
+public class HelloWorld {
+    // a method ( function ) called " main "
+    // parameter input: array of strings
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
