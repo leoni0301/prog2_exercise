099f6c42939bcc8d24796029d60f34846c2549d7
leoni0301 <38730364+leoni0301@users.noreply.github.com>
2018-05-16 18:25:05 +0200

    Add files via upload

---
+++ exercise_3/SentenceTester.java
@@ -0,0 +1,22 @@
+public class SentenceTester {
+    public static void main(String[] args) {
+
+        // create instances
+        Token Token1 = new Token(1,"Der","der","ART",2,"NK");
+        Token Token2 = new Token(2,"Mann", "Mann","NN",3,"SB");
+        Token Token3 = new Token(3,"hat", "haben", "VAFIN", 0,"");
+        Token[] firstSent = new Token[] {Token1, Token2, Token3};
+        Token[] secondSent = new Token[] {Token1, Token2, Token3};
+        TokenSentence Sent = new TokenSentence(firstSent);
+        TokenSentence otherSent = new TokenSentence(secondSent);
+
+        // Aufruf: Instanzmethode length
+        System.out.println("Der Satz beinhaltet " + Sent.length() + " Wörter.");
+        // Aufruf: Instanzmethode equals
+        System.out.println("Sind die Sätze äquivalent? " + Sent.equals(otherSent));
+        // Aufruf: Instanzmethode setToken
+        Sent.setToken(0, Token3);
+        // Erneutes prüfen auf equality
+        System.out.println("Sind die Sätze äquivalent? " + Sent.equals(otherSent));
+    }
+}
