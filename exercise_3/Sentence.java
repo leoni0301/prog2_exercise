f52d0c53af7f4754f3f1a1dd034841db027d718d
SimBusch <38330572+SimBusch@users.noreply.github.com>
2018-05-16 16:00:27 +0200

    Add files via upload

--- exercise_3/Sentence.java
+++ exercise_3/Sentence.java
@@ -3,73 +3,60 @@ import java.util.Scanner;
 
 public class Sentence {
 
-        // Instanzvariable vom Typ String-Array
-        private String[] tokens;
+	// Instanzvariable vom Typ String-Array
+	private String[] tokens;
 
-        // Konstruktor
-        Sentence ( String[] tokens ){
-            this.tokens = tokens;
-        }
+	// Konstruktor
+	Sentence ( String[] tokens ){
+		this.tokens = tokens;
+	}
 
-        // Instanzmethode length
-        private int length(){
-            return tokens.length;
-        }
+	// Instanzmethode length
+	private int length(){
+		return this.tokens.length; 	//Was ist hier der Unterschied zwischen tokens.length und this.tokens.length?
+	}
 
-        // Instanzmethode getToken
-        public String getToken(int i) {
-            String token = "";
-            for (int j = 0; j < tokens.length; j++) {
-                token += tokens[i];
-                break;
-            }
-            return token;
-        }
+	// Instanzmethode getToken
+	public String getToken(int i) {
+		return this.tokens[i];			//Vereinfacht
+	}
 
-        // Instanzmethode setToken
-        private void setToken(int i, String t){
-            for (int j = 0; j < this.tokens.length; j++) {
-                String token = this.tokens[i]; // Token at the position i
-                String sent = Arrays.toString(this.tokens);
-                String newSent = sent.replace(token,t);
-                System.out.println("Das Wort \"" + token + "\" wird durch \"" + t + "\" ersetzt: " + newSent);
-                break;
-            }
-        }
+	// Instanzmethode setToken
+	private void setToken(int i, String t){
+		this.tokens[i] = t;					//Alte Methode hat nicht das eigentliche Sentence-Objekt editiert
+	}
+	
 
-         // Instanzmethode equals
-        private boolean equals(Sentence otherSent){
+	// Instanzmethode equals
+	private boolean equals(Sentence otherSent){
+		
+		//So weit ich die Aufgabe lese sollen die Sätz nicht sortiert werden
+		boolean status = Arrays.equals(this.tokens, otherSent.tokens);
+		return status;
+	}
 
-            Arrays.sort(this.tokens);
-            Arrays.sort(otherSent.tokens);
+	
+	public static void main(String[] args) {
 
-            boolean status = Arrays.equals(this.tokens, otherSent.tokens);
-            return status;
-        }
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Enter a sentence: ");
+		String[] tokens1 = scanner.nextLine().split(" ");
+		scanner.close();
 
-        public static void main(String[] args) {
+		// create instances
+		Sentence Sent = new Sentence(tokens1);
+		String [] newSent = new String[] {"Das", "ist", "ein", "Satz"};
+		Sentence otherSent = new Sentence(newSent);
 
-            Scanner scanner = new Scanner(System.in);
-            System.out.println("Enter a sentence: ");
-            String[] tokens1 = scanner.nextLine().split(" ");
-            // System.out.println("Enter a sentence: ");
-            // String[] tokens2 = scanner.nextLine().split(" ");
-            scanner.close();
-
-            // create instances
-            Sentence Sent = new Sentence(tokens1);
-            String [] newSent = new String[] {"Das", "ist", "ein", "Satz"};
-            Sentence otherSent = new Sentence(newSent);
-
-            // Example: Der Mann hat angerufen, der gestern da war.
-            // Aufruf: Instanzmethode length
-            System.out.println("Der Satz beinhaltet " + Sent.length() + " WÃ¶rter.");
-            // Aufruf: Instanzmethode getToken
-            System.out.println("Das Wort \"" + Sent.getToken(1) + "\" befindet sich an der gesuchten Stelle.");
-            // Aufruf: Instanzmethode setToken
-            Sent.setToken(2, "Hallo");
-            // Aufruf: Instanzmethode equals
-            System.out.println("Sind die SÃ¤tze Ã¤quivalent? " + Sent.equals(otherSent));
-
-    }
+		// Aufruf: Instanzmethode length
+		System.out.println("Der Satz beinhaltet " + Sent.length() + " Wörter.");
+		// Aufruf: Instanzmethode getToken
+		System.out.println("Das Wort \"" + Sent.getToken(0) + "\" befindet sich an der gesuchten Stelle.");
+		// Aufruf: Instanzmethode equals
+		System.out.println("Sind die Sätze äquivalent? " + Sent.equals(otherSent));
+		// Aufruf: Instanzmethode setToken
+		Sent.setToken(2, "Hallo");
+		// Erneutes prüfen auf equality
+		System.out.println("Sind die Sätze äquivalent? " + Sent.equals(otherSent));
+	}
 }
