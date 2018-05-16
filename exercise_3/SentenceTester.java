da040774732013b2a374aabb8e9196e87e94c9c3
Katharina Jablockin <kj.ru@gmx.de>
2018-05-16 22:23:35 +0200

    Erstelle Methode testTokenSentence und füge den Code aus der main ein; erstelle Methode testSentence und teste die equals Methode von Sentence

--- exercise_3/SentenceTester.java
+++ exercise_3/SentenceTester.java
@@ -1,22 +1,47 @@
 public class SentenceTester {
-    public static void main(String[] args) {
 
-        // create instances
-        Token Token1 = new Token(1,"Der","der","ART",2,"NK");
-        Token Token2 = new Token(2,"Mann", "Mann","NN",3,"SB");
-        Token Token3 = new Token(3,"hat", "haben", "VAFIN", 0,"");
-        Token[] firstSent = new Token[] {Token1, Token2, Token3};
-        Token[] secondSent = new Token[] {Token1, Token2, Token3};
-        TokenSentence sent = new TokenSentence(firstSent);
-        TokenSentence otherSent = new TokenSentence(secondSent);
+  private static void testSentence(){
 
-        // Aufruf: Instanzmethode length
-        System.out.println("Der Satz beinhaltet " + sent.length() + " Wörter.");
-        // Aufruf: Instanzmethode equals
-        System.out.println("Sind die Sätze äquivalent? " + sent.equals(otherSent));
-        // Aufruf: Instanzmethode setToken
-        sent.setToken(0, Token3);
-        // Erneutes prüfen auf equality
-        System.out.println("Sind die Sätze äquivalent? " + sent.equals(otherSent));
-    }
+    // create instances
+    String[] firstSent = new String[]{"Der", "Mann", "hat"};
+    String[] secondSent = new String[]{"Der", "Mann", "hat"};
+    Sentence sent = new Sentence(firstSent);
+    Sentence otherSent = new Sentence(secondSent);
+
+    // Aufruf: Instanzmethode length
+    System.out.println("Der Satz beinhaltet " + sent.length() + " Wörter.");
+    // Aufruf: Instanzmethode equals
+    System.out.println("Sind die Sätze äquivalent? " + sent.equals(otherSent));
+    // Aufruf: Instanzmethode setToken
+    sent.setToken(1, "Hund");
+    // Erneutes prüfen auf equality
+    System.out.println("Sind die Sätze äquivalent? " + sent.equals(otherSent));
+  }
+
+  private static void testTokenSentence() {
+    // create instances
+    Token Token1 = new Token(1, "Der", "der", "ART", 2, "NK");
+    Token Token2 = new Token(2, "Mann", "Mann", "NN", 3, "SB");
+    Token Token3 = new Token(3, "hat", "haben", "VAFIN", 0, "");
+    Token[] firstSent = new Token[]{Token1, Token2, Token3};
+    Token[] secondSent = new Token[]{Token1, Token2, Token3};
+    TokenSentence sent = new TokenSentence(firstSent);
+    TokenSentence otherSent = new TokenSentence(secondSent);
+
+    // Aufruf: Instanzmethode length
+    System.out.println("Der Satz beinhaltet " + sent.length() + " Wörter.");
+    // Aufruf: Instanzmethode equals
+    System.out.println("Sind die Sätze äquivalent? " + sent.equals(otherSent));
+    // Aufruf: Instanzmethode setToken
+    sent.setToken(0, Token3);
+    // Erneutes prüfen auf equality
+    System.out.println("Sind die Sätze äquivalent? " + sent.equals(otherSent));
+  }
+
+  public static void main(String[] args) {
+
+    //testSentence();
+
+    testTokenSentence();
+  }
 }
