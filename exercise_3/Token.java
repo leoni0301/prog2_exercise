3b514c8e0bd159081148d5521c082f7badbc3657
leoni0301 <38730364+leoni0301@users.noreply.github.com>
2018-05-16 17:50:15 +0200

    Update Token.java

--- exercise_3/Token.java
+++ exercise_3/Token.java
@@ -1,5 +1,6 @@
 public class Token {
-
+        
+        // Instanzvariablen
         int id; // Position im Satz
         String surface; // orthographischen Wortform
         String lemma; // Grundform des Tokens
@@ -20,22 +21,13 @@ public class Token {
 
         }
         
-        //ist jetzt auf this bezogen
-        private boolean equals(Token token2) {
+       // Instanzmethode equals
+        private boolean equals(Token token) {
 
-            boolean status1 = this.surface.equals(token2.surface);
-            boolean status2 = this.lemma.equals(token2.lemma);
-            boolean status3 = this.pos.equals(token2.pos);
+            boolean status1 = this.surface.equals(token.surface);
+            boolean status2 = this.lemma.equals(token.lemma);
+            boolean status3 = this.pos.equals(token.pos);
             return status1 && status2 && status3;
-        }
-
-
-    public static void main(String[] args) {
-
-            Token token1 = new Token(1, "Mann", "Mann", "AR", 2, "NK");
-            Token token2 = new Token(2, "Mann", "Mann", "NN", 3, "SB");
-            // String [] Sent = new String[] {"Der", "Mann", "hat", "angerufen,", "der", "gestern", "da", "war"};
-            System.out.println(token1.equals(token2));
         }
 
 }
