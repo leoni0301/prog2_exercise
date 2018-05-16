e2c12d9058d1cb018775b2b1e31c2af41d4e102f
Katharina Jablockin <kj.ru@gmx.de>
2018-05-16 22:07:27 +0200

    Korrigiere Sentence equals Methode; Variablennamen in SentenceTester gemäß Konvention angepasst.

--- exercise_3/Sentence.java
+++ exercise_3/Sentence.java
@@ -2,34 +2,39 @@ import java.util.Arrays;
 
 public class Sentence {
 
-	// Instanzvariable vom Typ String-Array
-	private String[] tokens;
+  // Instanzvariable vom Typ String-Array
+  private String[] tokens;
 
-	// Konstruktor
-	Sentence ( String[] tokens ){
-		this.tokens = tokens;
-	}
+  // Konstruktor
+  Sentence(String[] tokens) {
+    this.tokens = tokens;
+  }
 
-	// Instanzmethode length
-	public int length(){
-		return this.tokens.length; //Ich glaube, dass er bei tokens.length auf die Instanzvariable zugreift und bei this.tokens auf den Konstruktor also ist this.tokens die bessere Variante
-	}
+  // Instanzmethode length
+  public int length() {
+    return this.tokens.length; //Ich glaube, dass er bei tokens.length auf die Instanzvariable zugreift und bei this.tokens auf den Konstruktor also ist this.tokens die bessere Variante
+  }
 
-	// Instanzmethode getToken
-	public String getToken(int i) {
-		return this.tokens[i];	
-	}
+  // Instanzmethode getToken
+  public String getToken(int i) {
+    return this.tokens[i];
+  }
 
-	// Instanzmethode setToken
-	public void setToken(int i, String t){
-		this.tokens[i] = t;	
-	}
-	
+  // Instanzmethode setToken
+  public void setToken(int i, String t) {
+    this.tokens[i] = t;
+  }
 
-	// Instanzmethode equals
-	public boolean equals(Sentence otherSent){
-		
-		//In der Aufgabenstellung stand "in der gleichen Reihenfolge" deshalb hab ich Array.sort verwendet" 
-		return Arrays.equals(this.tokens, otherSent.tokens);
-	}
+
+  // Instanzmethode equals
+  public boolean equals(Sentence otherSent) {
+
+    for (int i = 0; i < this.tokens.length; i++) {
+      if (this.getToken(i) != otherSent.getToken(i)) {
+        return false;
+      }
+    }
+
+    return true;
+  }
 }
--- exercise_3/SentenceTester.java
+++ exercise_3/SentenceTester.java
@@ -7,16 +7,16 @@ public class SentenceTester {
         Token Token3 = new Token(3,"hat", "haben", "VAFIN", 0,"");
         Token[] firstSent = new Token[] {Token1, Token2, Token3};
         Token[] secondSent = new Token[] {Token1, Token2, Token3};
-        TokenSentence Sent = new TokenSentence(firstSent);
+        TokenSentence sent = new TokenSentence(firstSent);
         TokenSentence otherSent = new TokenSentence(secondSent);
 
         // Aufruf: Instanzmethode length
-        System.out.println("Der Satz beinhaltet " + Sent.length() + " Wörter.");
+        System.out.println("Der Satz beinhaltet " + sent.length() + " Wörter.");
         // Aufruf: Instanzmethode equals
-        System.out.println("Sind die Sätze äquivalent? " + Sent.equals(otherSent));
+        System.out.println("Sind die Sätze äquivalent? " + sent.equals(otherSent));
         // Aufruf: Instanzmethode setToken
-        Sent.setToken(0, Token3);
+        sent.setToken(0, Token3);
         // Erneutes prüfen auf equality
-        System.out.println("Sind die Sätze äquivalent? " + Sent.equals(otherSent));
+        System.out.println("Sind die Sätze äquivalent? " + sent.equals(otherSent));
     }
 }
