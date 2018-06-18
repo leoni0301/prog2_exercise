51a94c3e7174526ba26d0fe012df7796713d5b00
Katharina Jablockin <kj.ru@gmx.de>
2018-06-18 23:33:38 +0200

    Setze den Konstruktor in Klasse Node als public; erstelle Klasse GraphWithAdjacencyList, implementiere Interface Graph und implementiere Methode containsEdge; modifiziere Klasse GraphTester, sodass dieser keinen Fehler ausspuckt beim Testen der ersten Methode;

--- exercise_8/graphs/GraphTester.java
+++ exercise_8/graphs/GraphTester.java
@@ -1,26 +1,28 @@
 package graphs;
 
 import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 
-public class GraphTester {    
+public class GraphTester {
     public static void main(String[] args) throws IOException, InvalidNodeException {
-        String filename = "../example_graph.txt";
-        
-        Graph withAdjacencyMatrix = new GraphWithAdjacencyMatrix(filename);
+        String filename = "./example_graph.txt";
+
+        //Graph withAdjacencyMatrix = new GraphWithAdjacencyMatrix(filename);
         Graph withAdjacencyList = new GraphWithAdjacencyList(filename);
         
         List<Graph> graphs = new ArrayList<>();
         List<String> names = new ArrayList<>();
         
-        graphs.add(withAdjacencyMatrix);
+        //graphs.add(withAdjacencyMatrix);
         graphs.add(withAdjacencyList);
         
         names.add("adjacency matrix");
         names.add("adjacency list");
         
-        for (int i = 0; i < 2; i++) {
+        for (int i = 0; i < 1; i++) {
             Graph g = graphs.get(i);
             String name = names.get(i);
             
--- exercise_8/graphs/GraphWithAdjacencyList.java
+++ exercise_8/graphs/GraphWithAdjacencyList.java
@@ -1,17 +1,53 @@
 package graphs;
 
-import java.util.Collection;
+import complexnodes.Edge;
+import complexnodes.Node;
+
+import java.io.IOException;
+import java.util.*;
+
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 public class GraphWithAdjacencyList implements Graph {
 
+  private HashMap<String, Node> nodes;
+  private ArrayList<Edge> edges;
+  private Integer nodeCount;
 
   public GraphWithAdjacencyList(String filename) {
-    
+
+    try {
+      List<String> lines = Files.readAllLines(Paths.get(filename));
+
+      nodeCount = 0;
+      nodes = new HashMap<>();
+      edges = new ArrayList<>();
+
+      Iterator<String> iter = lines.iterator();
+      while (iter.hasNext()) {
+        String line = iter.next();
+        String[] parts = line.split(" ");
+        if (lines.indexOf(line) > 0) {
+          // Andere Zeilen
+          edges.add(new Edge(nodes.get(parts[0]), nodes.get(parts[1]), parts[0] + " " + parts[1], 0));
+        } else {
+          // Erste Zeile
+          for (Integer i = 0; i < parts.length; i++) {
+            nodes.put(parts[i], new Node(parts[i], i));
+            nodeCount++;
+          }
+        }
+      }
+
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
   }
 
   @Override
   public boolean containsEdge(String i, String j) throws InvalidNodeException {
-    return false;
+    return edges.stream().anyMatch(e -> e.getFirstNode().getLabel().equals(i) && e.getSecondNode().getLabel().equals(j));
   }
 
   @Override
@@ -27,5 +63,9 @@ public class GraphWithAdjacencyList implements Graph {
   @Override
   public boolean addNode(String i) {
     return false;
+  }
+
+  public Integer getNodeCount() {
+    return nodeCount;
   }
 }
