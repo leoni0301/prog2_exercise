ecc45d47460eeb310cf86c08f4e0aa0a768a14c6
Katharina Jablockin <kj.ru@gmx.de>
2018-07-09 22:53:24 +0200

    Kommentiere Lambdas.

--- exercise_11/src/elementutils/Test.java
+++ exercise_11/src/elementutils/Test.java
@@ -2,17 +2,19 @@ package elementutils;
 
 public class Test {
   public static void main(String[] args) {
-    // int 1 größer int 2 ?
+    // pred1 checks whether an integer object is bigger than another
     TwoElementPredicate pred1 = (i, j) -> (int) i > (int) j;
     int result1 = (int) ElementUtils.betterElement(1, 3, pred1);
     System.out.println(result1);
 
-    // string 1 kürzer string 2 ?
-    String result2 = (String) ElementUtils.betterElement("blub", "bla", (i, j) -> ((String) i).length() < ((String) j).length());
+    // pred2 checks whether a string object is shorter than another
+    TwoElementPredicate pred2 = (i, j) -> ((String) i).length() < ((String) j).length();
+    String result2 = (String) ElementUtils.betterElement("blub", "bla", pred2);
     System.out.println(result2);
 
-    // bool 1 ungleich bool 2 ?
-    boolean result3 = (boolean) ElementUtils.betterElement(true, false, (i, j) -> (boolean) i != (boolean) j);
+    // pred3 checks whether a boolean object is the same as another
+    TwoElementPredicate pred3 = (i, j) -> (boolean) i != (boolean) j;
+    boolean result3 = (boolean) ElementUtils.betterElement(true, false, pred3);
     System.out.println(result3);
   }
 }
